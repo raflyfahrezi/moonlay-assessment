756d5b89675312529bec09bf53bbf5a3aa043115
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-14 18:50:36 +0700

    fix people modules

--- src/modules/people/index.tsx
+++ src/modules/people/index.tsx
@@ -1,15 +1,43 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
 
-import { Header, Search, Wrapper } from '@/components'
+import { toJS } from 'mobx'
+import React, { useEffect } from 'react'
+import { observer } from 'mobx-react-lite'
+
+import { IPeople } from '@/store'
+import { useStore } from '@/hooks'
+import { Card, Header, Search, Wrapper, ModuleWrapper } from '@/components'
+
+import { peopleCardBoxClassName } from './styles'
 
 const PeopleModule = () => {
+    const store = useStore()
+
+    const fetchData = async () => {
+        await store.getPeople()
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [store])
+
+    useEffect(() => {
+        console.log(toJS(store.people))
+    }, [store.people])
+
     return (
         <Wrapper>
             <Header title='People' />
-            <Search />
-            <div></div>
+            <ModuleWrapper>
+                <Search />
+                <div className={peopleCardBoxClassName}>
+                    {toJS(store.people).map((person: IPeople) => {
+                        return <Card key={person.name} {...person} />
+                    })}
+                </div>
+            </ModuleWrapper>
         </Wrapper>
     )
 }
 
-export default PeopleModule
+export default observer(PeopleModule)
