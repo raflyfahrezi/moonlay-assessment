81a603b39339b354f68a6f372277b3d741f9b3e2
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-14 18:48:56 +0700

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ const tailwindConfig = {
             gridTemplateRows: {
                 pageWrapper: '1fr auto;',
             },
+            gridTemplateColumns: {
+                cardBox: 'repeat(auto-fill, minmax(250px, 1fr));',
+            },
         },
     },
     plugins: [],
