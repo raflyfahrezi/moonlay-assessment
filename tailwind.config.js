69b5761f56e7dc5be780a4cba402dde0f607a572
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-14 07:36:37 +0700

    initialize tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+const tailwindConfig = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
+module.exports = tailwindConfig
