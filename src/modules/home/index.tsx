fb49c38e0f163738516fb38308c1a1e6bd150f1c
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-14 10:18:32 +0700

    fix home modules

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { Wrapper } from '@/components'
+import { StarIcon, PeopleIcon, PlanetIcon } from '@/assets'
+
+import Card from './card'
+import { Menu } from './types'
+import {
+    HomeModuleClassName,
+    HomeModuleTitleClassName,
+    HomeModuleCardsClassName,
+} from './styles'
 
 const HomeModule = () => {
-    return <div>HomeModule</div>
+    const [menus] = useState<Menu[]>([
+        {
+            title: 'People',
+            icon: <PeopleIcon />,
+        },
+        {
+            title: 'Planets',
+            icon: <PlanetIcon />,
+        },
+        {
+            title: 'Starships',
+            icon: <StarIcon />,
+        },
+    ])
+
+    return (
+        <Wrapper>
+            <div className={HomeModuleClassName}>
+                <div className={HomeModuleTitleClassName}>
+                    <p>Moonlay Assessment</p>
+                </div>
+                <div className={HomeModuleCardsClassName}>
+                    {menus.map((item) => {
+                        const { title, icon } = item
+
+                        return <Card key={title} title={title} icon={icon} />
+                    })}
+                </div>
+            </div>
+        </Wrapper>
+    )
 }
 
 export default HomeModule
---
+++ src/modules/home/types.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from 'react'
+
+export interface Menu {
+    title: string
+    icon: ReactNode
+}
