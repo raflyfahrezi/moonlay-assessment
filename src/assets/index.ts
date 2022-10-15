39cc680eec58b2e5f3327fc2af20b480f2d7719a
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-15 10:50:43 +0700

    adding new assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,7 @@
 import BackIcon from './back'
 import StarIcon from './star'
+import XmarkIcon from './xmark'
 import PlanetIcon from './planet'
 import PeopleIcon from './people'
 
-export { BackIcon, StarIcon, PlanetIcon, PeopleIcon }
+export { BackIcon, StarIcon, XmarkIcon, PlanetIcon, PeopleIcon }
