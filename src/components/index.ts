fd3119896dea7d96aac729456efd43b5b8b2e21e
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-15 10:19:59 +0700

    fix components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,8 @@ import Button from './button'
 import Header from './header'
 import Search from './search'
 import Wrapper from './wrapper'
+import CardWrapper from './cardWrapper'
 import ModuleWrapper from './moduleWrapper'
 
-export { Card, Button, Header, Search, Wrapper, ModuleWrapper }
+export { Card, Button, Header, Search, Wrapper, CardWrapper, ModuleWrapper }
 export * from './form'
