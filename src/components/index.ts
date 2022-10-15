352de5bd332e21ddcc923fe63794c70f8456060b
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-15 10:50:57 +0700

    create modal components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import Card from './card'
+import Modal from './modal'
 import Button from './button'
 import Header from './header'
 import Search from './search'
@@ -6,5 +7,14 @@ import Wrapper from './wrapper'
 import CardWrapper from './cardWrapper'
 import ModuleWrapper from './moduleWrapper'
 
-export { Card, Button, Header, Search, Wrapper, CardWrapper, ModuleWrapper }
+export {
+    Card,
+    Modal,
+    Button,
+    Header,
+    Search,
+    Wrapper,
+    CardWrapper,
+    ModuleWrapper,
+}
 export * from './form'
---
+++ src/components/modal/index.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+
+import { XmarkIcon } from '@/assets'
+import { Wrapper } from '@/components'
+
+import { ModalProps } from './types'
+
+const Modal = ({ title, isOpen, children, onClose }: ModalProps) => {
+    useEffect(() => {
+        document.body.style.overflow = isOpen ? 'hidden' : 'unset'
+    }, [isOpen])
+
+    return (
+        <>
+            {isOpen && (
+                <div className='fixed w-full min-h-screen py-10 bg-black bg-opacity-60 flex justify-center z-50'>
+                    <Wrapper>
+                        <div className='w-full h-full p-6 bg-gray-900 rounded flex flex-col'>
+                            <div className='w-full flex justify-between items-center'>
+                                <div>
+                                    <p className='font-bold text-lg'>{title}</p>
+                                </div>
+                                <div>
+                                    <button
+                                        onClick={onClose}
+                                        className='p-2 bg-gray-600 rounded active:scale-95'
+                                    >
+                                        <XmarkIcon />
+                                    </button>
+                                </div>
+                            </div>
+                            <div className='pt-10'>{children}</div>
+                        </div>
+                    </Wrapper>
+                </div>
+            )}
+        </>
+    )
+}
+
+export default Modal
